321be79216b335b8778db9102caf47e79d2cbb99
PeterMonev <monevpeter@gmail.com>
2023-11-06 14:28:28 +0200

    Creating Logout functionality

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,22 @@
 import '../Header/Header.css';
+import { useAuth } from '../../hooks/authContext';
 
 import { Link } from 'react-router-dom';
 
-
 export const Header = () => {
+  const { token , logout} = useAuth();
+
+  const handleLogout = () => {
+    logout();
+  }
+
     return(
     <header className="header">
       <nav className="nav">
         <ul className="nav__ul">
           <Link to='/login'>Login</Link>
           <Link to='/register'>Register</Link>
-          <Link to='/logout'>Logout</Link>
+          {token && <Link to='/logout' onClick={handleLogout}>Logout</Link>}
           <Link to='/dashboard'>Dashboard</Link>
           <Link to='/cars'>Cars</Link>
         </ul>
